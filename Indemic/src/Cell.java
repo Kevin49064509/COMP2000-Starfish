6f89b27aa5be1bb05c537b9962553943ee83e59b
Kevin <kevin.zhou@students.mq.edu.au>
2026-09-16 12:43:16 +1000

    Update Cell.java
    added damage variable, constructor and ability to damage boss

--- Indemic/src/Cell.java
+++ Indemic/src/Cell.java
@@ -1,3 +1,11 @@
 public class Cell {
+    private int damage;
 
+    public Cell(int damage) {
+        this.damage = damage;
+    }
+
+    public void applyDamage(Boss boss) {
+        boss.takeDamage(damage);
+    }
 }
