4a73e66b909f3b2be04dffd4a77b9c23b10c6dd4
Kevin <kevin.zhou@students.mq.edu.au>
2026-09-16 16:15:57 +1000

    Added subclasses
    subclasses for cell, one class does more damage, one collects stuff

---
+++ Indemic/src/collectorCell.java
@@ -0,0 +1,8 @@
+public class collectorCell extends Cell {
+    // Doing damage to boss will have resources come off which will be collected by these cells.
+    @Override 
+    public collectorCell(int damage) {
+        super(damage);
+    }
+
+}
