03f48be0dbc9030c178f4d544a0918321f1de206
Kevin <kevin.zhou@students.mq.edu.au>
2026-09-16 12:27:12 +1000

    Big bad boss virus health
    set maxhealth and current heal variables

--- Indemic/src/Boss.java
+++ Indemic/src/Boss.java
@@ -1,3 +1,11 @@
 public class Boss {
+    private int health;
+    private int maxHealth;
 
+    // also add attack later
+
+    public Boss(int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+    }
 }
